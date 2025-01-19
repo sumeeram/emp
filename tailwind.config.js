05ef046c4ffd84ab4f9c5e1c718f6a918ec065dc
sumeeram <sumeeram@syntaxgenie.com>
2025-01-19 22:59:03 +0530

    changed color

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,13 +6,11 @@ export default {
       colors: {
         primary: {
           DEFAULT: "#012116", // Base color
-          light: "#81C784", // Lighter shade
-          dark: "#388E3C", // Darker shade
+          light: "#1A3A3C", // Lighter shade
         },
         danger: {
           DEFAULT: "#F44336",
           light: "#E57373",
-          dark: "#D32F2F",
         },
         transparent: "transparent",
       },
