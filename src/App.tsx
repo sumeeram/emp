d1266419b3c192e10d0b71a1ddf95a0be7306fa3
sumeeram <sumeeram@syntaxgenie.com>
2025-01-19 23:22:50 +0530

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 export default function App() {
   return (
-    <>
+    <div className="container text-center mx-auto mt-60">
       <h1 className="text-5xl font-bold underline">Hello world!</h1>
-      <div className="space-y-4">
+      <div className="mt-10 grid grid-cols-4 gap-4">
         <button className="btn btn-primary">Primary Button</button>
         <button className="btn btn-danger">Danger Button</button>
         <button className="btn btn-transparent">Transparent Button</button>
       </div>
-    </>
+    </div>
   );
 }
